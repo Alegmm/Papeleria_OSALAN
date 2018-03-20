b5b87fa624e7049758c89489559f18e639ed00cc
OskarCali <joscali98@micorreo.upp.edu.mx>
2018-03-19 21:12:42 -0600

    Add SplashScreen

---
+++ Papeleria/Papeleria/SplashScreen.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Papeleria
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar.Location = new System.Drawing.Point(0, 415);
+            this.progressBar.Maximum = 99;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(800, 35);
+            this.progressBar.TabIndex = 0;
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreen";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.SplashScreen_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# Papeleria_OSALAN
-Proyecto de Programación Visual sobre una Papelería
+# Papeleria OSALAN
+Proyecto de Programación Visual sobre una Papelería.  
+[Integrantes](#Desarrolladores)
+
+## Descripcion
+Aplicacion para el control de una papeleria.
+
+* [Splash Screen](#Splash-Screen)
+* [Log-In](#Log-In)
+* [Inicio](#Inicio)
+	1. [Administrador](#Administrador)
+	2. [Empleado](#Empleado)
+
+## Aplicacion
+### Splash Screen
+Splash Screen de la aplicacion con un Progress Bar.
+
+### Log-In
+Log-In de la aplicacion con un usuario administrador o un empleado.  
+Si es la primera ejecucion del programa el primer usuario en hacer el log-in sera considerado el usuario administrador.
+
+### Inicio
+Inicio distinto segun el tipo de usuario que se inicio sesion.
+
+#### Administrador
+* [Añadir Empleado](#Añadir-Empleado)
+* [Compra](#Añadir-Compra)
+* [Historial](#Historial)
+* [Empleados](#Empleados)
+* [Venta](#Venta)
+* [Almacen](#Almacen)
+
+#### Empleado
+* [Venta](#Venta)
+
+#### Añadir Empleado
+Añadir un empleado al registro de la papeleria.  
+
+#### Añadir Compra
+Añadir las compras de productos para la papeleria.  
+
+#### Historial
+Historial de ventas y compras para la papeleria.  
+Posibilidad de generar PDF.
+
+#### Empleados
+Registro de los empleados que existen en la papeleria.  
+Posibilidad de generar PDF.
+
+#### Venta
+Generar venta de articulos al publico.  
+Posibilidad de generar Ticket.
+
+#### Almacen
+Registro de los productos que se tienen en existencia.  
+Posibilidad de generar PDF.
+
+
+### Desarrolladores
+__Oscar Josue Lira Calva__ *OskarCalí*  
+__Alejandra Garcia Morales__  
+__Angel de Jesus Taboada Valencia__
